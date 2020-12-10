5cc0b5b2316acfa4a82ae99f0e79ccaeda94aa7e
Apekshya Khanal <apekshyanischal@Apekshyas-MacBook-Air.local>
2020-12-10 08:26:15 -1000

    worked on singup.jsx

--- front/src/welcome/Signup.jsx
+++ front/src/welcome/Signup.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React from "react";
 
 const Signup = (props) => {
-    return (
-        <div>
-
-        </div>
-    )
-}
+  return (
+    <div>
+      <p>Signup</p>
+      <form>
+        <label className="signup-label">Username: </label>
+        <input
+          type="text"
+          name="username"
+          onChange={props.onChangeSignUp}
+        ></input>{" "}
+        <br />
+        <label className="signup-label">Email:</label>
+        <input
+          type="email"
+          name="email"
+          onChange={props.onChangeSignUp}
+        ></input>{" "}
+        <br />
+        <label className="signup-label">Password: </label>
+        <input
+          type="password"
+          name="password"
+          onChange={props.onChangeSignUp}
+        ></input>{" "}
+        <br />
+        <label className="signup-label">Confirm Password: </label>
+        <input
+          type="password"
+          name="password"
+          onChange={props.onChangeSignUp}
+        ></input>{" "}
+        <br />
+        <button type="submit">Submit</button>
+        <br />
+        <button type="cancel">Cancel</button>
+      </form>
+    </div>
+  );
+};
 
 export default Signup;
--- front/src/welcome/Welcome.jsx
+++ front/src/welcome/Welcome.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import Login from './Login.jsx';
-import Signup from './Signup.jsx';
-import NewsFeed from '../inner-components/newsfeed/NewsFeed.jsx';
-import SearchBar from '../inner-components/search/SearchBar.jsx';
+import React from "react";
+import Login from "./Login.jsx";
+import Signup from "./Signup.jsx";
+import NewsFeed from "../inner-components/newsfeed/NewsFeed.jsx";
+import SearchBar from "../inner-components/search/SearchBar.jsx";
 
 const Welcome = (props) => {
-    return (
-        <div>
-            <h1>{props.username}</h1>
-            <div>
-                <Login onChangeLogin={props.onChange}/>
-                <Signup />
-                <NewsFeed />
-                <SearchBar onChangeSearch={props.onChange}/>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>{props.username}</h1>
+      <div>
+        <Login onChangeLogin={props.onChange} />
+        <Signup onChangeSignUp={props.onChange} />
+        <NewsFeed />
+        <SearchBar onChangeSearch={props.onChange} />
+      </div>
+    </div>
+  );
+};
 
 export default Welcome;
