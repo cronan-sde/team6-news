cda1ac6cf02962d9c9f5bffe33ca2625c9d2a4e7
Cody Cronan <codycronberger@gmail.com>
2020-12-10 08:42:05 -0800

    adding routes folder

---
+++ back/models/user.model.js
@@ -0,0 +1,38 @@
+//create mongoose user schema
+const mongoose = require('mongoose');
+require('mongoose-type-email');
+
+//type designates type of data coming in
+//unique designates something must be unique and not already used
+//required designates a required field
+//trim will remove leading and trailing whitespace
+//select: false will keep that data from being sent back from db
+//minlength: 4 means must be 4 chars or more
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    minlength: 4
+  },
+  email: {
+    type: mongoose.SchemaTypes.Email,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true,
+    trim:true,
+    select: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+const User = mongoose.model('User', UserSchema);
+//export userSchema
+module.exports = User;
