0e9b91bb1b90ab6b67df4d151a632187010129f8
Cody Cronan <codycronberger@gmail.com>
2020-12-14 14:49:54 -0800

    user passwords are now hashed, and validated

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -5,23 +5,30 @@ const bcrypt = require('bcryptjs');
 //returns json for valid user if name and pass are matches in db
 //returns error if no match on name or pass
 //populates the bookmarks array with the articles stored in db for use on front-end
-exports.user_login = (req, res) => {
+module.exports.user_login = async (req, res) => {
   const {username, password} = req.body;
 
-  User.findOne({
-    username,
-    password
-  })
-  //populates bookmarks with article, hids bookmarkedBy array
-  .populate("bookmarks", "-bookmarkedBy") 
+  //getting user matching username and getting their password from db
+  const user = await User.findOne({username}).select('password');
+  //if no match, return error
+  if (!user) {
+    return res.status(400).json("Error: Invalid username/password");
+  }
+  //check entered password against hashed db password
+  const isPasswordMatch = await bcrypt.compare(password, user.password);
+  //if the passwords don't match, return error
+  if (!isPasswordMatch) {
+    return res.status(400).json("Error: Invalid username/password");
+  }
+
+  //If passwords matched return the user
+  User.findOne({username}).populate("bookmarks", "-bookmarkedBy")
     .then(user => {
-      if (user) {
-        return res.json(user);
-      }
-      return res.status(400).json("Error: Invalid username/password");
-    })
+      return res.json(user);
+    });
 }
 
+//ensures all proper fields are filled out, no duplicates in db and hashes password for safer storage in db
 exports.user_signup = async (req, res) => {
   //getting user info
   const {username, email, password, createdAt} = req.body;
