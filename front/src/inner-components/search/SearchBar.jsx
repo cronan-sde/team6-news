4054593c07c552fef5c6757875711d60814d70ae
Apekshya Khanal <apekshyanischal@Apekshyas-MacBook-Air.local>
2020-12-15 19:48:14 -1000

    navbar

--- front/src/inner-components/search/SearchBar.jsx
+++ front/src/inner-components/search/SearchBar.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    '& .MuiTextField-root': {
+    "& .MuiTextField-root": {
       margin: theme.spacing(1),
-      width: 'auto',
-      margin: 'auto',
+      width: "auto",
+      margin: "auto",
       marginLeft: 100,
       marginRight: 10,
     },
@@ -22,14 +22,26 @@ const SearchBar = (props) => {
   return (
     <form className={classes.root} noValidate autoComplete="off">
       <TextField
-        id="outlined-basic" 
-        label="Search News" 
+        id="outlined-basic"
+        label="Search News"
         variant="outlined"
         type="text"
         name="searchBar"
-        onChange={(event) => {props.onChangeSearch(event)}}
+        onChange={(event) => {
+          props.onChangeSearch(event);
+        }}
       />
-      <Button variant="contained" style={{backgroundColor: "#66799b", color: "#e5e3e3", marginRight: 100, marginTop: 9}} onClick={()=> props.onSubmitSearch()} type="button">
+      <Button
+        variant="contained"
+        style={{
+          backgroundColor: "#66799b",
+          color: "#e5e3e3",
+          marginRight: 100,
+          marginTop: 9,
+        }}
+        onClick={() => props.onSubmitSearch()}
+        type="button"
+      >
         Submit
       </Button>
     </form>
--- front/src/navbar/WelcomeNavBar.jsx
+++ front/src/navbar/WelcomeNavBar.jsx
@@ -1,24 +1,139 @@
-import React from 'react';
-import SearchBar from '../inner-components/search/SearchBar.jsx';
-import Logo from './navbar-components/Logo.jsx';
-import LoginBtn from './navbar-components/LoginBtn.jsx';
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import Logo from "./navbar-components/Logo.jsx";
+import Button from "@material-ui/core/Button";
+import LoginBtn from "./navbar-components/LoginBtn.jsx";
 
-import { AppBar, Toolbar } from '@material-ui/core';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
 
 const WelcomeNavBar = (props) => {
-    return (
-        <AppBar position="sticky" className="navbar" style={{backgroundColor: "#989ba1"}}>
-            <Toolbar>
-                <Logo />
-                <SearchBar 
-                    onChangeSearch={props.onChangeSearch}
-                    onSubmitSearch={props.onSubmitSearch}
-                />
-                <LoginBtn userHasClicked={props.userHasClicked} />
-            </Toolbar>
-            {/* <SignupBtn userHasClicked={props.userHasClicked} /> */}
-        </AppBar>
-    )
-}
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Logo className={classes.menuButton} />
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search News"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              name="searchBar"
+              onChange={(event) => {
+                props.onChangeSearch(event);
+              }}
+              inputProps={{ "aria-label": "search" }}
+            />
+          </div>
+          <Button
+            variant="contained"
+            style={{
+              backgroundColor: "#66799b",
+              color: "#e5e3e3",
+              marginRight: 100,
+              marginTop: 9,
+            }}
+            onClick={() => props.onSubmitSearch()}
+            type="button"
+          >
+            Submit
+          </Button>
+          <LoginBtn userHasClicked={props.userHasClicked} />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
 
 export default WelcomeNavBar;
+
+// import React from 'react';
+// import SearchBar from '../inner-components/search/SearchBar.jsx';
+// import Logo from './navbar-components/Logo.jsx';
+// import LoginBtn from './navbar-components/LoginBtn.jsx';
+
+// import { AppBar, Toolbar } from '@material-ui/core';
+
+// const WelcomeNavBar = (props) => {
+//     return (
+//         <AppBar position="sticky" className="navbar" style={{backgroundColor: "#989ba1"}}>
+//             <Toolbar>
+//                 <Logo />
+//                 <SearchBar
+//                     onChangeSearch={props.onChangeSearch}
+//                     onSubmitSearch={props.onSubmitSearch}
+//                 />
+//                 <LoginBtn userHasClicked={props.userHasClicked} />
+//             </Toolbar>
+//             {/* <SignupBtn userHasClicked={props.userHasClicked} /> */}
+//         </AppBar>
+//     )
+// }
+
+// export default WelcomeNavBar;
